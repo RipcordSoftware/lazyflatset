ceeeee9a013ad63b82fdfd623b143ef2cabec223
craigminihan <craig@ripcordsoftware.com>
2015-06-18 22:56:11 +0100

    Added shell test project

---
+++ test/lazyflatset/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: craig
+ *
+ * Created on 18 June 2015, 22:50
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
