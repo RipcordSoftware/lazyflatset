b25bf4d82aa7d7e59335100734188ad8ad257e73
craigminihan <craig@ripcordsoftware.com>
2016-09-28 17:21:27 +0100

    Enabled clang builds on linux

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,5 +1,3 @@
-make all CXX=g++-4.9 && make test CXX=g++-4.9 && ./coverage.sh gcov-4.9
-
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     make CC=${_CC} CXX=${_CXX} all && make CC=${_CC} CXX=${_CXX} test && ./coverage.sh ${_COV}
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
