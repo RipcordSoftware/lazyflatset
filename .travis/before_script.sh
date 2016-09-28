64f73afa05d251429ba2998d7e453c4375484614
craigminihan <craig@ripcordsoftware.com>
2016-09-28 17:10:13 +0100

    Moved travis event scripts to a sub-directory

---
+++ .travis/before_script.sh
@@ -0,0 +1,10 @@
+mkdir -p .travis.temp
+pushd .travis.temp
+
+curl http://ftp.uk.debian.org/debian/pool/main/l/lcov/lcov_1.11.orig.tar.gz -O
+tar xfz lcov_1.11.orig.tar.gz
+mkdir -p lcov && make -C lcov-1.11/ install PREFIX=~/lcov
+export PATH=~/lcov/usr/bin:$PATH
+gem install coveralls-lcov
+
+popd
